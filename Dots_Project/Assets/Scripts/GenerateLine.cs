19ea21fbfe9f1d773b0535975d7f67143347ab4c
IvanovaPolina <ip.96@mail.ru>
2018-11-10 03:16:50 +0400

    Написала класс генерации линии. На поле 3х3 работает без ошибок. Линия строится по соседним клеткам и не пересекает саму себя. На поле 4х4 всё не так радужно, работает через раз. Есть ошибка в 79 строке скрипта, которую следует исправить.

--- Dots_Project/Assets/Scripts/GenerateLine.cs
+++ Dots_Project/Assets/Scripts/GenerateLine.cs
@@ -14,13 +14,26 @@ namespace Game
 		/// </summary>
 		public List<Cell> Path { get; private set; }
 
+		/// <summary>
+		/// Черный список для каждой точки пути. Содержит клетки, которые ранее приводили линию в тупик
+		/// </summary>
+		private List<List<Cell>> blackList = new List<List<Cell>>();
+
 		#region Check
 		public TrailRenderer line;
+		private TrailRenderer trail;
 		private void Start() {
-			NewLine(6);
-			TrailRenderer trail = Instantiate(line);
+			trail = Instantiate(line);
 			trail.enabled = false;
-			StartCoroutine(DrawLine(trail, 0.02f, 1f));
+			StartCoroutine(Test());
+		}
+		IEnumerator Test() {
+			int testCount = Field.Instance.GameField.Length;
+			while (true) {
+				yield return new WaitForSeconds(1f);
+				NewLine(testCount);
+				yield return StartCoroutine(DrawLine(trail, 0.02f, 1f));
+			}
 		}
 		#endregion
 
@@ -33,63 +46,17 @@ namespace Game
 			if (count < 2) count = 2;
 			else if (count > gameField.Length) count = gameField.Length;
 			List<Cell> path = new List<Cell>(count);
+			blackList.Clear();
 
 			// спавним первую точку в рандомной позиции
 			int indexI = Random.Range(0, gameField.GetUpperBound(0) + 1);
 			int indexJ = Random.Range(0, gameField.GetUpperBound(1) + 1);
 			Cell firstPoint = gameField[indexI, indexJ];
 			path.Add(firstPoint);
-			firstPoint.ChangeColor(Color.blue);
-			
-			int countWithoutLostCorners = gameField.Length - 3;	// Кол-во клеток поля минус углы, которые могут быть потеряны
-			RestPoints(path, count - path.Count, countWithoutLostCorners);
+			blackList.Add(new List<Cell>());	// создаем для первой точки черный список
 
-			// строим линию
-			#region Test
-			//List<Cell> restCells = new List<Cell>(gameField.Length);    // создаем список оставшихся клеток
-			//for (int i = 0; i < gameField.GetLength(0); i++)
-			//	for (int j = 0; j < gameField.GetLength(1); j++)
-			//		restCells.Add(gameField[i, j]);
-
-			//for (int i = 0; i < count; i++) {       // формируем путь
-			//	Cell temp = restCells[Random.Range(0, restCells.Count)];
-			//	path.Add(temp);
-			//	restCells.Remove(temp);
-			//}
-			#endregion
-
+			BuildRestPoints(path, count - path.Count);	// достраиваем остальные клетки
 			Path = path;
-		}
-
-		/// <summary>
-		/// Проверяет последнюю точку пути на отсечение угловой точки поля. Если проверка не пройдена - заменяет точку на новую
-		/// </summary>
-		/// <param name="firstNeighbours">Соседние точки предпоследней точки пути</param>
-		/// <param name="secondPoint">Последняя точка пути</param>
-		private Cell CheckForEmptyCorners(List<Cell> firstNeighbours, Cell secondPoint) {
-			#region Description
-			// следует сразу избежать ситуации, когда генерация точек пойдет при подобных условиях:
-			// count = 9, firstPoint = gameField[0, 1], secondPoint = gameField[1, 0]
-			// таким образом мы теряем точку gameField[0, 0], и будем строить дальнейший путь зря (и потеряем время)
-			// задача: отыскать точку в углу, которая могла бы оказаться отсеченной при неправильном выборе второй точки пути
-			// решение: если первая и вторая точка имеют общего соседа (таких может быть два в нашем случае), 
-			// который находится в углу (а значит имеет только трёх соседей, два из которых - это первая и вторая точка)
-			// в таком случае меняем вторую точку на какую-либо другую
-			#endregion
-			bool find = false;  // флаг, определяющий, нашли ли мы точку, подходящую под наши условия
-			while (!find) {
-				List<Cell> secondNeighbours = new List<Cell>(secondPoint.Neighbours);
-				foreach (var n in firstNeighbours) {
-					if (secondNeighbours.Contains(n) && n.Neighbours.Count == 3) {
-						firstNeighbours.Remove(secondPoint);
-						secondPoint = firstNeighbours[Random.Range(0, firstNeighbours.Count)];
-						if (secondPoint.Neighbours.Count == 8) return secondPoint;
-						find = false;
-						break;
-					} else find = true;
-				}
-			}
-			return secondPoint;
 		}
 
 		/// <summary>
@@ -98,51 +65,47 @@ namespace Game
 		/// <param name="path">Начатый путь</param>
 		/// <param name="count">Количество точек, которые осталось задействовать в построении</param>
 		/// <param name="countWithoutLostCorners">Величина, необходимая для проверки на отсечение угловых точек. = кол-во клеток поля минус углы, которые могут быть потеряны</param>
-		private void RestPoints(List<Cell> path, int count, int countWithoutLostCorners) {
-			// найти соседей последней точки из пути
-			// если в этих соседях имеются точки пути - удалить из соседей эти точки
-			// выбрать рандомную точку в этих соседях
-			// проверка: если пусть содержит больше 3-х точек, значит надо делать проверку на пересечение с путём!
-			// если нет - идем дальше. проверяем, построен ли путь (count == 0). Если да - return;
-			// если (count > 0) строим еще точку
-
-			// обернуть весь метод в цикл while (count > 0). Это исключит рекурсию.
-			while (count > 0) {
+		private void BuildRestPoints(List<Cell> path, int count) {
+			while (count > 0) {     // если осталось 0 клеток - путь построен
 				Cell tail = path[path.Count - 1];   // последний элемент в пути
 				List<Cell> tailNeighbours = new List<Cell>(tail.Neighbours);
-				//if(tailNeighbours.Count == 0) {
-				//	count++;
-				//	path.Remove(tail);
-				//	continue;
-				//}
+				// очищаем от соседних клеток, которые уже есть в пути
 				ClearNeighboursFromPathCells(tailNeighbours, path);
+				// очищаем от соседних клеток, которые ведут к пересечению линии самой себя
+				ClearNeighboursFromIntersects(tailNeighbours, path);
+				// очищаем от соседних клеток, которые уже были задействованы ранее и привели линию в тупик
+				ClearNeighboursFromBlackList(tailNeighbours, path);
+				if (tailNeighbours.Count == 0) {	// если после всех очисток соседних клеток не осталось
+					blackList[path.Count - 2].Add(tail);	// заносим последнюю клетку в черный список предпоследней клетки 
+					count++;
+					path.Remove(tail);
+					continue;	// запускаем цикл while заново
+				}	// если соседние клетки остались - строим путь дальше
 				Cell next = tailNeighbours[Random.Range(0, tailNeighbours.Count)];
-				if(path.Count + count > countWithoutLostCorners)	// если путь обязан задействовать угловые клетки
-					next = CheckForEmptyCorners(tailNeighbours, next);	// не допускаем отсечение углов последней точкой пути
-
-				if(path.Count >= 3) {
-					// делаем проверку на пересечение отрезков (next; path[path.Count - 1]) и остальных отрезков пути
-					// если проверка не пройдена - continue;
-					// если пройдена - идем дальше
-					bool intersect = false;
-					for (int i = 0; i < path.Count - 1; i++) {
-						if (!IsIntersect(next.transform.position, tail.transform.position, path[i].transform.position, path[i + 1].transform.position)) {
-							intersect = true;
-							break;
-						}
-					}
-					if (intersect) continue;
-				}
 				path.Add(next);
 				count--;
+				blackList.Add(new List<Cell>());	// создаем для этой клетки черный список
+			}
+		}
+
+		/// <summary>
+		/// Очищает список соседей от всех клеток, которые были задействованы ранее и привели линию в тупик
+		/// </summary>
+		private void ClearNeighboursFromBlackList(List<Cell> tailNeighbours, List<Cell> path) {
+			int tailIndex = path.Count - 1;		// индекс последней клетки из пути
+			if (blackList[tailIndex].Count > 0) {	// если в черном списке есть соседи этой клетки, которые приводили ранее линию в тупик
+				foreach (var item in blackList[tailIndex])	// очищаем список соседей от уже ненужных клеток
+					tailNeighbours.Remove(item);
+				// если последний индекс черного списка больше, чем последний индекс пути (если в blackList записано больше точек, чем осталось в пути)
+				// очищаем лист от использованных точек, для того, чтобы в дальнейшем его переиспользовать
+				if (blackList.Count - 1 > tailIndex)	// if(4 - 1 > 2)
+					blackList.RemoveRange(tailIndex + 1, blackList.Count - 1 - tailIndex);	// RemoveRange(3, 1);
 			}
 		}
 
 		/// <summary>
 		/// Убирает из списка соседей все клетки, которые уже есть в построенном пути
 		/// </summary>
-		/// <param name="neighbours">Список соседних клеток</param>
-		/// <param name="path">Построенный путь</param>
 		private void ClearNeighboursFromPathCells(List<Cell> neighbours, List<Cell> path) {
 			for (int i = 0; i < neighbours.Count; i++) {
 				if (path.Contains(neighbours[i])) {
@@ -153,17 +116,34 @@ namespace Game
 		}
 
 		/// <summary>
-		/// Определяет, пересекаются ли отрезки AB и CD
+		/// Убирает из списка соседей все клетки, при соединении с которыми линия пересекает саму себя
 		/// </summary>
-		private bool IsIntersect(Vector2 A, Vector2 B, Vector2 C, Vector2 D) {
-			float v1 = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
-			float v2 = (D.x - C.x) * (B.y - C.y) - (D.y - C.y) * (B.x - C.x);
-			float v3 = (B.x - A.x) * (C.y - A.y) - (B.y - A.y) * (C.x - A.x);
-			float v4 = (B.x - A.x) * (D.y - A.y) - (B.y - A.y) * (D.x - A.x);
-			return ((v1 * v2 <= 0) && (v3 * v4 <= 0));
+		private void ClearNeighboursFromIntersects(List<Cell> neighbours, List<Cell> path) {
+			if (path.Count < 3) return;     // для линии из < 3 точек нет смысла производить расчеты
+			// берем последнюю точку пути, соединяем с соседней точкой - получаем один отрезок
+			// берем по очереди отрезок пути (2 последовательные точки) - это второй отрезок
+			// сравниваем эти два отрезка на пересечение. В случае пересечения удаляем эту соседнюю точку
+			for (int i = 0; i < neighbours.Count; i++) {	// перебираем соседние точки
+				for (int j = 0; j < path.Count - 2; j++) {	// перебираем точки из пути (последние две не берем, т.к. они образовывают угол с отрезком, содержащим соседнюю точку)
+					if (IsIntersect(path[path.Count - 1], neighbours[i], path[j], path[j + 1])) {
+						neighbours.RemoveAt(i);
+						i--;
+						break;
+					}
+				}
+			}
 		}
 
-
+		/// <summary>
+		/// Определяет, пересекаются ли отрезки AB и CD
+		/// </summary>
+		private bool IsIntersect(Cell A, Cell B, Cell C, Cell D) {
+			float v1 = (D.I - C.I) * (A.J - C.J) - (D.J - C.J) * (A.I - C.I);
+			float v2 = (D.I - C.I) * (B.J - C.J) - (D.J - C.J) * (B.I - C.I);
+			float v3 = (B.I - A.I) * (C.J - A.J) - (B.J - A.J) * (C.I - A.I);
+			float v4 = (B.I - A.I) * (D.J - A.J) - (B.J - A.J) * (D.I - A.I);
+			return ((v1 * v2 <= 0) && (v3 * v4 <= 0));
+		}
 
 		/// <summary>
 		/// Рисует линию, сгенерированную компьютером
@@ -194,38 +174,42 @@ namespace Game
 			trail.enabled = false;
 		}
 
-		#region IntersectsTest
-		//private void IntersectsTest() {
-		//	Debug.Log("В скобках указано значение, которое должно получиться");
+		#region Intersects_Test
+		/// <summary>
+		/// Проверяет два отрезка на пересечение (рассчитано для поля 3х3 и больше)
+		/// </summary>
+		private void IntersectsTest() {
+			Cell[,] field = Field.Instance.GameField;
+			Debug.Log("В скобках указано значение, которое должно получиться");
 
-		//	// для пересекающихся отрезков
-		//	Vector2 A = new Vector2(1, 2);  // точка A
-		//	Vector2 B = new Vector2(5, 6);  // точка B
-		//	Vector2 C = new Vector2(2, 4);  // точка C
-		//	Vector2 D = new Vector2(5, 3);  // точка D
-		//	Debug.Log("Пересекающиеся отрезки (True): " + IsIntersect(A, B, C, D));
+			// для пересекающихся отрезков
+			Cell A = field[0, 0];  // клетка A
+			Cell B = field[1, 1];  // клетка B
+			Cell C = field[0, 1];  // клетка C
+			Cell D = field[1, 0];  // клетка D
+			Debug.Log("Пересекающиеся отрезки (True): " + IsIntersect(A, B, C, D));
 
-		//	// для параллельных отрезков
-		//	A = new Vector2(1, 1);
-		//	B = new Vector2(1, 4);
-		//	C = new Vector2(3, 2);
-		//	D = new Vector2(3, 4);
-		//	Debug.Log("Параллельные (False): " + IsIntersect(A, B, C, D));
+			// для параллельных отрезков
+			A = field[0, 0];
+			B = field[1, 0];
+			C = field[0, 1];
+			D = field[1, 1];
+			Debug.Log("Параллельные (False): " + IsIntersect(A, B, C, D));
 
-		//	// для непересекающихся отрезков
-		//	A = new Vector2(1, 3);
-		//	B = new Vector2(3, 4);
-		//	C = new Vector2(2, 2);
-		//	D = new Vector2(3, 1);
-		//	Debug.Log("Непересекающиеся (False): " + IsIntersect(A, B, C, D));
+			// для непересекающихся отрезков
+			A = field[0, 0];
+			B = field[1, 1];
+			C = field[2, 1];
+			D = field[1, 2];
+			Debug.Log("Непересекающиеся (False): " + IsIntersect(A, B, C, D));
 
-		//	// для отрезков, у которых одна координата - общая
-		//	A = new Vector2(1, 2);
-		//	B = new Vector2(2, 3);
-		//	C = new Vector2(2, 3);
-		//	D = new Vector2(3, 5);
-		//	Debug.Log("С одной общей координатой (True): " + IsIntersect(A, B, C, D));
-		//}
+			// для отрезков, у которых одна координата - общая
+			A = field[0, 0];
+			B = field[0, 1];
+			C = field[0, 1];
+			D = field[1, 1];
+			Debug.Log("С одной общей координатой (True): " + IsIntersect(A, B, C, D));
+		}
 		#endregion
 	}
 }
